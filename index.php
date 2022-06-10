650ecb4e3a6bfe18642ba86b46290ed1105d9778
Zenrool <lorenzopapperini@hotmail.it>
2022-06-10 06:56:50 +0200

    Header

--- index.php
+++ index.php
@@ -7,11 +7,46 @@
     <!-- Titolo  -->
     <title>Google policies faq</title>
     <!-- Foglio di stile  -->
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <?php
+    <header>
+        <div class = "d-flex justify-between align-center">
+            <a href="#" class="d-flex align-center">
+                <img alt="Qries" src="google.png">
+                <span>Privacy e termini</span>
+            </a>
+            <div class = "d-flex align-center">
+                <div class="quadre">
+                    <div>.</div>
+                    <div>.</div>
+                    <div>.</div>
+                    <div>.</div>
+                    <div>.</div>
+                    <div>.</div>
+                    <div>.</div>
+                    <div>.</div>
+                    <div>.</div>
+                </div>
+                <div class="circle">L</div>
+            </div>
+        </div>
+        <ul class="d-flex">
+            <?php
+            $nav = [
+                "Introduzione",
+                "Norme sulla privacy",
+                "Termini di servizio",
+                "Tecnologie",
+                "Domande frequenti",
+            ];
+            foreach($nav as $voice) {
+                echo "<li>" . $voice . "</li>";
+            }
 
-    ?>
+            ?>
+        </ul>
+    </header>
+
 </body>
 </html>
